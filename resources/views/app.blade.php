5fa6a1b8fcabc7af45da01499443d25093b56013
aderamadhana <sanade2034@gmail.com>
2025-12-11 10:05:51 +0700

    update favicon dan  title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/images/logo_kecil.png') }}">
+    <title>PT. Mitra Wira Mas HRIS</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
