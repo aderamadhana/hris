a59066d3ef09e9d0c69d36b74b59563bd38fbdf6
aderamadhana <sanade2034@gmail.com>
2026-01-27 16:46:06 +0700

    update sejarah

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -157,6 +157,60 @@
                                 publishing software like Aldus PageMaker
                                 including versions of Lorem Ipsum.
                             </p>
+                            <ul class="mini-list">
+                                <li>
+                                    <font-awesome-icon icon="check" />
+                                    <div>
+                                        <strong>Lorem Ipsum</strong>
+                                        <span
+                                            >Lorem Ipsum is simply dummy text of
+                                            the printing and typesetting
+                                            industry. Lorem Ipsum has been the
+                                            industry's standard dummy text ever
+                                            since the 1500s, when an unknown
+                                            printer took a galley of type and
+                                            scrambled it to make a type specimen
+                                            book. It has survived not only five
+                                            centuries, but also the leap into
+                                            electronic typesetting, remaining
+                                            essentially unchanged. It was
+                                            popularised in the 1960s with the
+                                            release of Letraset sheets
+                                            containing Lorem Ipsum passages, and
+                                            more recently with desktop
+                                            publishing software like Aldus
+                                            PageMaker including versions of
+                                            Lorem Ipsum.</span
+                                        >
+                                    </div>
+                                </li>
+                                <li>
+                                    <font-awesome-icon icon="check" />
+                                    <div>
+                                        <strong>Lorem Ipsum</strong>
+                                        <span
+                                            >Lorem Ipsum is simply dummy text of
+                                            the printing and typesetting
+                                            industry. Lorem Ipsum has been the
+                                            industry's standard dummy text ever
+                                            since the 1500s, when an unknown
+                                            printer took a galley of type and
+                                            scrambled it to make a type specimen
+                                            book. It has survived not only five
+                                            centuries, but also the leap into
+                                            electronic typesetting, remaining
+                                            essentially unchanged. It was
+                                            popularised in the 1960s with the
+                                            release of Letraset sheets
+                                            containing Lorem Ipsum passages, and
+                                            more recently with desktop
+                                            publishing software like Aldus
+                                            PageMaker including versions of
+                                            Lorem Ipsum.</span
+                                        >
+                                    </div>
+                                </li>
+                            </ul>
                         </div>
                     </div>
 
